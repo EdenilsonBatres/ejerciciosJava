1aaa5fe9854ecaec04ed8440b29b7c179471f0b2
Edenilson <edenilson.batres@gmail.com>
2022-04-25 21:20:14 -0600

    ejercicio uno de la guia

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args)
     {
-        int sumaNumeros = sumarnumeros(1, 2,3);
-        System.out.println(sumaNumeros);
+        int sumaNumeros = sumarnumeros(3, 300,-3);
+        System.out.println("Los numeros sumados mediante la funcion son :"+sumaNumeros);
     }
 
     public static int sumarnumeros (int a, int b, int c){
