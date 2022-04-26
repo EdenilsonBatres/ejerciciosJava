462c5756b7dbda112de18321d56d4a257983c501
Edenilson <edenilson.batres@gmail.com>
2022-04-25 21:07:01 -0600

    ejercicio uno de la guia

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int sumaNumeros = sumarnumeros(1, 2,3);
+        System.out.println(sumaNumeros);
+    }
+
+    public static int sumarnumeros (int a, int b, int c){
+
+      int resultado = a + b + c;
+        return resultado ;
+    }
+
+}
