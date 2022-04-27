a3d753dcae92bd29c701914e0c86fa7b54e8fc83
Edenilson <edenilson.batres@gmail.com>
2022-04-26 21:52:50 -0600

    se agrego el segundo ejercicio del tema tres

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args)
     {
         int sumaNumeros = sumarnumeros(3, 300,-3);
         System.out.println("Los numeros sumados mediante la funcion son :"+sumaNumeros);
+        coche miCoche = new coche();
+        miCoche.incrementandoPuertas(2);
+        System.out.println("cantidad de puertas del coche :"+ miCoche.cantidadPuertas);
     }
 
     public static int sumarnumeros (int a, int b, int c){
---
+++ src/coche.java
@@ -0,0 +1,13 @@
+public class coche {
+    int cantidadPuertas = 0;
+
+    public int incrementandoPuertas(int numeroPuertas )
+    {
+        int totalPuertas = 0;
+        totalPuertas = totalPuertas+ numeroPuertas;
+        cantidadPuertas = totalPuertas;
+        return totalPuertas;
+    }
+
+    //cantidadPuertas = incrementandoPuertas(totalPuertas);
+}
